b67d57a38d80372174b464450e488faea502bde4
fengahan <544976880@qq.com>
2019-03-12 18:02:38 +0800

    add config

---
+++ common/config/main-local.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    'language'=>'zh-CN',
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=192.168.182.147;dbname=yuan_dian',
+            'username' => 'root',
+            'password' => '123456',
+            'charset' => 'utf8',
+            'tablePrefix'=>'yy_',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
